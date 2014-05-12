5b9e463529954540e9a2566d94d1c19b31c6822a
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-05-12 15:45:13 -0500

    populate Symtype on deploy

--- lib/Quiver/Schema.pm
+++ lib/Quiver/Schema.pm
@@ -15,6 +15,37 @@ __PACKAGE__->load_namespaces;
 # Created by DBIx::Class::Schema::Loader v0.07039 @ 2014-01-19 14:48:51
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:2Gjssjl+sssOS+FPRdlXvQ
 
+use Try::Tiny;
+use Moo;
 
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
+after deploy => sub {
+	my ($schema) = @_;
+	$schema->populate_symtype;
+};
+
+=method populate_symtype()
+
+Adds default symbol types to the Symtype table.
+
+=cut
+sub populate_symtype {
+	my ($schema) = @_;
+	my $rs;
+	try {
+		$rs = $schema->txn_do(sub {
+			$schema->resultset('Symtype')->populate([
+				[ qw(symtypeid name) ],
+				[ 100, 'function definition' ],
+				[ 101,  'function prototype' ],
+				[ 102,             'comment' ],
+				[ 103,       'documentation' ],
+				[ 104,               'macro' ],
+			]);
+		});
+	} catch {
+		my $error = shift;
+		$error->rethrow;
+	};
+}
+
 1;
